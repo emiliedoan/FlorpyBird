59d766637672467628866aa18e752cf92038080f
Emilie Doan <emiliecdoan@gmail.com>
2025-03-11 23:01:09 -0400

    game works!
    still need to add game over when out of bounds, and exit button
    but ugly game over menu exists 💯

---
+++ florpy birb/Assets/logicscript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class logicscript : MonoBehaviour
+{
+    public int playerScore = 0;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("AddScore")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
